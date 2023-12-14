d3cfaba0a830641bf608909be133f0aae70d62e1
Wisfie <wiz.fie@gmail.com>
2023-12-14 09:46:45 +0700

    ff

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -9,7 +9,7 @@
 					<h2>The page you are looking for doesn't exist.</h2>
 					<a class="btn" href="/">Back to home</a>
 					<img
-						src="assets/img/not-found.svg"
+						src="./assets/img/not-found.svg"
 						class="img-fluid py-5"
 						alt="Page Not Found"
 					/>
